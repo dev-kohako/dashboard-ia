2f6f38b7e071341d6e38be5c8346f10362e40459
kohako_dev <kohako.tech@gmail.com>
2025-04-28 10:20:20 -0300

    feat: middlewares and routes delete

--- backend/src/routes/auth.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { register, login } from '../controllers/authController';
-
-export const authRoutes = Router();
-
-authRoutes.post('/register', register);
-authRoutes.post('/login', login);
