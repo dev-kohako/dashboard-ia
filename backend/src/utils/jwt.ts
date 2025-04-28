2357fb6e8ec9760a8327b1e5ef07cae2e1d72723
kohako_dev <kohako.tech@gmail.com>
2025-04-28 10:21:24 -0300

    feat: creating and reviewing jwt.ts

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -2,6 +2,6 @@ import jwt from 'jsonwebtoken'
 
 export function generateToken(userId: string): string {
   return jwt.sign({ userId }, process.env.JWT_SECRET!, {
-    expiresIn: '1d'
+    expiresIn: '7d'
   })
 }
