fa430fdc0fe51a3e27e0939387bac1adb573a0f4
kohako_dev <kohako.tech@gmail.com>
2025-04-29 02:57:37 -0300

    feat: updating db logic

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,12 +3,14 @@ import { typeDefs } from './schemas/auth.schema'
 import { resolvers } from './resolvers/auth.resolver'
 import { createContext } from './utils/context'
 
+const port = process.env.PORT || 4000
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: createContext
 })
 
-server.listen({ port: process.env.PORT }).then(({ url }) => {
+server.listen({ port }).then(({ url }) => {
   console.log(`🚀 Servidor GraphQL rodando em ${url}`)
 })
