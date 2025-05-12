2dba1160677e097632847a2c306ebbbe3e2499f9
kohako_dev <kohako.tech@gmail.com>
2025-05-12 03:07:25 -0300

    feat: creating dashboard layout

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function Dashboard() {
+  return (
+ 
+    <div className="flex flex-1 flex-col">
+        <div className="@container/main min-h-screen flex flex-1 flex-col justify-center items-center  gap-2">
+            Hello Wolrd!
+        </div>
+      </div>
+
+  );
+}
